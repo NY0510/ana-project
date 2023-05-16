44c87eef1501ecf15388961e216a2d51b4714a06
NY64 <me@ny64.kr>
2023-05-16 09:38:22 +0900

    ✨ 메인 페이지

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+require("dotenv").config();
+const path = require("path");
 const express = require("express");
-const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`App is listening on port ${port}!`));
+global.__rootPath = path.resolve(process.cwd());
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/index"));
+app.listen(port, () => console.log(`App is listening on port ${port}!\n\nhttp://localhost:${port}`));
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+	res.sendFile(path.join(__rootPath, "views", "index.html"));
+});
 
 module.exports = router;
